0e60268283195d1434c103d5b80ec3f4edada7b7
Scott Diekema <scott.diekema@gmail.com>
2022-03-12 16:13:26 -0500

    music slider + video card

---
+++ script.js
@@ -0,0 +1,68 @@
+function menuSelect() {
+
+  const musicButton = document.querySelector("#button-music")
+  const videoButton = document.querySelector("#button-video")
+  const picsButton = document.querySelector("#button-pics")
+  const showsButton = document.querySelector("#button-shows")
+
+  const contentBox = document.querySelectorAll(".js-content-box")
+
+  contentBox[1].style.display = 'grid'
+
+  musicButton.addEventListener("click", function () {
+    contentBox[1].style.display = 'none'
+    contentBox[0].style.display = 'grid'
+  })
+
+  videoButton.addEventListener("click", function () {
+    contentBox[0].style.display = 'none'
+    contentBox[1].style.display = 'grid'
+  })
+
+}
+
+menuSelect()
+
+
+function musicSlider() {
+
+  let musicCards = document.querySelectorAll(".js-music-card")
+  const leftArrow = document.querySelector("#arrowleft-music")
+  const rightArrow = document.querySelector("#arrowright-music")
+
+  let i = 0;
+  musicCards[i].style.display = 'flex'
+
+
+  leftArrow.addEventListener("click", function () {
+    if (i >= 1) {
+      musicCards[i].style.display = 'none';
+      i--;
+      musicCards[i].style.display = 'flex';
+    }
+    else {
+      musicCards[i].style.display = 'none';
+      i += (musicCards.length - 1);
+      musicCards[i].style.display = 'flex';
+    }
+  })
+
+  rightArrow.addEventListener("click", function () {
+    if (i < (musicCards.length - 1)) {
+      musicCards[i].style.display = 'none';
+      i++;
+      musicCards[i].style.display = 'flex';
+    }
+    else {
+      musicCards[i].style.display = 'none';
+      i -= (musicCards.length - 1);
+      musicCards[i].style.display = 'flex';
+    }
+  })
+
+}
+
+musicSlider()
+
+
+
